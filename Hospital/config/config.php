5478ecbc6b7985b268744ce9c114c57e254f001a
jomalugarcia <jomalugarcia@gmail.com>
2026-03-13 12:48:17 -0700

    Delete Hospital directory

--- Hospital/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config/config.php
-
-session_start();
-
-define('SISTEMA_NOMBRE', 'Sistema Hospitalario');
-define('BASE_URL', '/hospital/');
--- Hospital/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'config/config.php';
-require_once 'includes/auth.php';
-
-header("Location: dashboard.php");
-exit;
